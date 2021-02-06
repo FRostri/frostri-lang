6cb1610cfba19069f55c4b8c8d0ae6f6396baad0
FRostri <FRostri@protonmail.com>
2021-02-06 18:18:53 -0500

    feat: Add test for ast and managin Expressions

--- frl/parser.py
+++ frl/parser.py
@@ -1,9 +1,17 @@
+# Calling IntEnum for make the precedence (this is like you are doing a math
+# expression an you have 2 + 4 * 3 - (3 - 1) when first we resolve the 
+# operation in the paren and then the product and so on to the end)
+from enum import IntEnum
 from typing import (
+    Callable,
+    Dict,
     Optional,
     List,
 )
 
 from frl.ast import (
+    Expression,
+    ExpressionStatement,
     Identifier,
     LetStatement,
     ReturnStatement,
@@ -17,6 +25,22 @@ from frl.token import (
 )
 
 
+PrefixParseFn = Callable[[], Optional[Expression]]
+InfixParseFn = Callable[[Expression], Optional[Expression]]
+PrefixParseFns = Dict[TokenType, PrefixParseFn]
+InfixParseFns = Dict[TokenType, InfixParseFn]
+
+
+class Precedence(IntEnum):
+    LOWEST = 1
+    EQUALS = 2
+    LESSGREATER = 3
+    SUM = 4
+    PRODUCT = 5
+    PREFIX = 6
+    CALL = 7
+
+
 class Parser:
 
     def __init__(self, lexer: Lexer) -> None:
@@ -24,6 +48,9 @@ class Parser:
         self._current_token: Optional[Token]
         self._peek_token: Optional[Token] = None
         self._errors: List[str] = []
+
+        self._prefix_parse_fns: PrefixParseFns = self._register_prefix_fns()
+        self._infix_parse_fns: InfixParseFns = self._register_infix_fns()
 
         self._advance_tokens()
         self._advance_tokens()
@@ -59,11 +86,40 @@ class Parser:
         self._expected_token_error(token_type)
         return False
 
+    def _parse_expression_statement(self) -> Optional[ExpressionStatement]:
+        assert self._current_token is not None
+        expression_statement = ExpressionStatement(token=self._current_token)
+
+        expression_statement.expression = self._parse_expression(Precedence.LOWEST)
+
+        assert self._peek_token is not None
+        if self._peek_token.token_type == TokenType.SEMICOLON:
+            self._advance_tokens()
+
+        return expression_statement
+
     def _expected_token_error(self, token_type: TokenType) -> None:
         assert self._peek_token is not None
         error = f'Se esperaba que el siguiente token fuera {token_type} ' + \
                 f'pero se obtuvo {self._peek_token.token_type}'
         self._errors.append(error)
+
+    def _parse_expression(self, precedence: Precedence) -> Optional[Expression]:
+        assert self._current_token is not None
+        try:
+            prefix_parse_fn = self._prefix_parse_fns[self._current_token.token_type]
+        except KeyError:
+            return None
+
+        left_expression = prefix_parse_fn()
+
+        return left_expression
+
+    def _parse_identifier(self) -> Identifier:
+        assert self._current_token is not None
+
+        return Identifier(token=self._current_token,
+                          value=self._current_token.literal)
 
     def _parse_let_statement(self) -> Optional[LetStatement]:
         assert self._current_token is not None
@@ -72,8 +128,7 @@ class Parser:
         if not self._expected_token(TokenType.IDENT):
             return None
 
-        let_statement.name = Identifier(
-            token=self._current_token, value=self._current_token.literal)
+        let_statement.name = self._parse_identifier()
 
         if not self._expected_token(TokenType.ASSIGN):
             return None
@@ -103,4 +158,12 @@ class Parser:
         elif self._current_token.token_type == TokenType.RETURN:
             return self._parse_return_statement()
         else:
-            return None
+            return self._parse_expression_statement()
+
+    def _register_infix_fns(self) -> InfixParseFns:
+        return {}
+
+    def _register_prefix_fns(self) -> PrefixParseFns:
+        return {
+            TokenType.IDENT: self._parse_identifier,
+        }
